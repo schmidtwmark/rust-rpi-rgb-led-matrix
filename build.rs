cc1ed194d643cf117070deb56d7ec1a695f75fe8
Mark Schmidt <schmidtwmark@gmail.com>
2020-08-22 08:12:32 -0500

    Fix build

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate gcc;
 
-fn main () {
+fn main() {
     println!("cargo:rustc-flags=-l dylib=stdc++");
 
-        println!("cargo:rustc-link-search=/home/pi/rpi-rgb-led-matrix/lib");
+    println!("cargo:rustc-link-search=/home/pi/rust-scoreboard/rust-rpi-rgb-led-matrix/rpi-rgb-led-matrix/lib");
 }
